b594c80f5016ac65c2cd2c30a6d406e4c4722b85
Moparthipremchand <chandprem06976@gmail.com>
2025-09-18 15:38:31 +0530

    Add files via upload

---
+++ logical_operators.py
@@ -0,0 +1,38 @@
+a=10
+b=5
+print(a>3 and b<10)
+
+x=7
+print(not (x < 10))
+
+x=4
+y=12
+print(x < 5 or y < 10)
+
+age=18
+has_id=False
+print(age >= 18 and has_id)
+
+
+logged_in=True
+admin=False
+print(logged_in or admin)
+
+is_sunny=False
+have_umbrella =True
+print(not is_sunny and have_umbrella)
+
+
+c=True
+d=False
+print(not (c or d))
+
+temp=25
+print((temp >20 and temp < 30))
+
+marks=45
+print(not(marks >=50 or marks ==49 ))
+
+e=0
+f=10
+print(not x and y > 5)
