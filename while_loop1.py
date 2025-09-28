fffb3493617baa7d37b9f9abc26c6cab685e2630
Moparthipremchand <chandprem06976@gmail.com>
2025-09-28 22:29:29 +0530

    Add files via upload

---
+++ while_loop1.py
@@ -0,0 +1,121 @@
+##print numbers from 100 to 1 using wile loop
+n=100
+while n>=0:
+    print(n)
+    n+=-1
+    
+    
+##print numbers from 1 to 100 using while loop
+n=1
+while n<=100:
+    print(n)
+    n+=1
+    
+## write a program to print numbers from 15 to 25
+n=15
+while n<=25:
+    print(n)
+    n+=1
+    
+    
+    
+##print even numbers from 1 to 100
+n=1
+while n<=100:
+    if n%2==0:
+        print(n)
+    n+=1
+    
+    
+    
+   ##print sum of even numbers between 1 to 100
+n=1
+total=0
+while n<=100:
+    if n%2==0:
+        # print(n)
+        total+=n
+    n+=1
+print(total)
+
+
+
+
+
+##print numbers from 20 to 1
+n=20
+while n>=1:
+    print(n)
+    n-=1
+    
+    
+##print odd numbers from 1 to 100
+n=1
+while n<=100:
+    if n%2==1:
+        print(n)
+    n+=1
+    
+    
+    
+##print sum of odd numbers from 1 o 100
+n=1
+sumodd=0
+while n<=100:
+    if n%2==1:
+        sumodd+=n
+    n+=1
+print(sumodd)
+
+
+
+
+##print numbers from 1 to 100 using while loop
+n=100
+while n>=1:
+    print(n)
+    n-=1
+    
+    
+    
+##write a program using while and while_else
+n=100
+while n>=1:
+    if n%2==0:
+        print(n,"even numbers")
+    n-=1
+else:
+    print("loop completed success fully")
+    
+    
+##print numbers from -1 to -10
+n=-1
+while n>=-10:
+    print(n)
+    n-=1
+    
+##print numbers from -100 to -1
+n=-100
+while n<=-1:
+    print(n)
+    n+=1
+    
+    
+    
+    
+##print numbers from-1 to -100 using for loop
+for i in range(-1,-100,-1):
+    print(i)
+    
+    
+##print numbers from -100 to -1 using for loop
+for i in range(-100,0,1):
+    print(i)
+    
+    
+    
+##print numbers from -100 to -1 using while loop
+n=-100
+while n<=-1:
+    print(n)
+    n+=1
